7970daa68ba9de5cc170d693d6c89cc05e926727
Abhay <abhayhmgi@gmail.com>
2023-06-07 13:41:19 +0530

    Introduction

---
+++ introduction/hello_world.cpp
@@ -0,0 +1,28 @@
+// Single line comment
+/* Multi line
+   Comment. */
+
+/* Comments are only for readability and are discarded(removed by compiler)
+   in preprocessing(compile) time. */
+
+/* This is to include standard stream IO facilities.
+   Without this below statement:
+   std::cout<<"Hello, World!"<<std::endl;
+   would make no sense. */
+#include <iostream>
+
+/* There should be only one global main function.
+   Execution starts from main always.
+   Return integer type value.
+   0 is returned on successful completion.
+   non 0 value represents failure. */
+int main(){
+
+    /* "<<"(put to) operater writes
+       its second argument to its first.
+       */
+    std::cout<<"Hello, World!\n";
+
+    // Returns 0 on successful completion.
+    return 0;
+}
